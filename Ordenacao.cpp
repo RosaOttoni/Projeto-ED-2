5c34442951bda4cac82dce22cc8183f469f128f2
Lucia Pereira <luhelenapereira@gmail.com>
2021-12-20 23:06:30 -0300

    Corrigindo as movimentações e comparação do tim

--- Ordenacao.cpp
+++ Ordenacao.cpp
@@ -70,7 +70,7 @@ void insertionSort(Review *vetor[], int inicio, int fim,  int &comparacao, int &
     {
         aux = vetor[i];
         int j=i-1;
-        //comparacao++;
+        movimentacao++;
         //deslocando os elementos anteriores a chave que sao maiores 
         while (j>0 && vetor[j]->getUpvotes() > aux->getUpvotes())
         {
@@ -80,6 +80,7 @@ void insertionSort(Review *vetor[], int inicio, int fim,  int &comparacao, int &
         }
         vetor[j+1] = aux;
         movimentacao++;
+        comparacao++;
     }
 }
 void intercala(Review *vetor[],int esq, int meio,int dir,  int &comparacao, int &movimentacao)
@@ -93,10 +94,12 @@ void intercala(Review *vetor[],int esq, int meio,int dir,  int &comparacao, int 
     for (i = 0; i < vet1; i++)
     {
         vetorEsq[i] = vetor[esq + i];
+        movimentacao++;
     }
     for (j = 0; j < vet2; j++)
     {
         vetorDir[j] = vetor[meio +1+j];
+        movimentacao++;
     }
     i =0,j=0;
     int k = esq;            
@@ -118,23 +121,21 @@ void intercala(Review *vetor[],int esq, int meio,int dir,  int &comparacao, int 
 
         }
         k++;
-        //comparacao++;
+        comparacao++;
      }
     while (i<vet1)
     {
         vetor[k] = vetorEsq[i];
-        movimentacao++;
         i++;
         k++;
-        comparacao++;
+        movimentacao++;
     }
     while (j<vet2)
     {
         vetor[k] = vetorDir[j];
         j++; 
-        movimentacao++;
-        comparacao++;
         k++;
+        movimentacao++;
     }
             
 }
