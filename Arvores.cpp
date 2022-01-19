ff8c75ae742bcbf55aae21336c011f6a9447bbb3
Lucia Pereira <luhelenapereira@gmail.com>
2022-01-19 14:01:47 -0300

    alteração em teste e analise

--- Arvores.cpp
+++ Arvores.cpp
@@ -15,9 +15,10 @@ void Arvores::analise(int n){
 
 
    int comparacoes;
-   n=10; // 100 registros
+   n=10;
    for(int cont=1; cont<n; cont++)
    {
+
        cout << "Execução e resultados da Arvore Vermelho e Preta: " << cont << "..." << endl;
         
         fstream arq;
@@ -25,54 +26,63 @@ void Arvores::analise(int n){
         
         if(arq)
         {
-                  //Criando arquivo para salvar estatísticas de busca
+         //Criando arquivo para salvar estatísticas de busca
+            ofstream arqSaidaI;
+            arqSaidaI  << "saidaBusca" + cont ; 
+            arqSaidaI.open("saidaInsercao.txt");
+
+            if (arqSaidaI)
+            {
                 ofstream saidaBusca;
-                saidaBusca  << "saidaBusca" + cont ;
-                saidaBusca.open("saida.txt");
+                saidaBusca  << "saidaBusca" + cont ; 
+                saidaBusca.open("saidaBusca.txt");
                 
                 if(saidaBusca)
                 {
-                    //Para e busca nas árvores
+                    //Para busca nas árvores
                     while(!arq.eof())
-                    {
 
-                         ArvoreVermelhoPreto *arvoreVP = new ArvoreVermelhoPreto();
+                    {
+                        ArvoreVermelhoPreto *arvoreVP;
                         
-                         ReviewNode **vet = new ReviewNode*[n];
+                        ReviewNode **vet = new ReviewNode*[n];
                          
-                         
-                        // cout<<"Buscando..";                 
+                        // cout<<"Buscando..";              
 
                         
                         high_resolution_clock::time_point inicio = high_resolution_clock::now();
-                         
+                        saidaBusca << " \nTamanho do conjunto: N: " << n << endl; 
+                        comparacoes = 0;
                             
-                            saidaBusca << " \nTamanho do conjunto: N: " << n << endl; //Imprime o tamanho do vetor no arquivo de saída da busca
-                            comparacoes = 0;
-                            
-                        //Buscando na Árvore Vermelha e Preta                        
-                        
-                            for(int i=0; i<n; i++)
-                            arvoreVP->busca(vet[i], comparacoes);
+                        //Buscando na Árvore Vermelha e Preta
+                        for(int i=0; i<n; i++)
+                        arvoreVP->insere(vet[i], comparacoes);
                         auto resultado = high_resolution_clock::now() - inicio;
                         double tempo = duration_cast<nanoseconds >(resultado).count();
+                        arqSaidaI << "Arvore-VP: \n" << "Tempo: " << tempo/1000000000 << "\nComparações: " << comparacoes << endl;
 
+                        comparacoes = 0;
                         
-
+                        for(int i=0; i<n; i++)
+                        arvoreVP->busca(vet[i], comparacoes);
+                        resultado = high_resolution_clock::now() - inicio;
+                        tempo = duration_cast<nanoseconds >(resultado).count();                     
                         saidaBusca << "Arvore-VP: \n" << "Tempo: " << tempo/1000000000 << "\nComparações: " << comparacoes << endl;
 
 
                         delete []vet;
                     }
-
                     saidaBusca.close();
-
-                }else
-                cout << "Erro ao criar o arquivo de saida da insercao" << endl;
-
+                }
+                else
+                    cout << "Erro ao criar o arquivo de saida da insercao" << endl;
+                    arqSaidaI.close();
+            }
+            else
+            cout << "" << endl;
             arq.close();
         }
         else
-            cout << "Erro ao abrir arquivo de entrada" << endl;
-    }
+         cout << "Erro ao abrir arquivo de entrada" << endl;
+   }
 }
--- main.cpp
+++ main.cpp
@@ -23,30 +23,17 @@ void imprimeMenu(){
 
 int main(int argc, char *argv[])
 {
-    int comparacoes, n = 10, totalComparacoes, opcao;// n = 1000000
-
+   int comparacoes, n = 5, totalComparacoes, opcao,op2;//n =1000000
     imprimeMenu();
 
     cin>> opcao;
 
-    while(opcao != 3){
-        switch(opcao){
-
-            case 1:{ // arvore vermelho e preta
-                ArvoreVermelhoPreto *arvoreVP = new ArvoreVermelhoPreto();
-
-                ReviewNode **vet = new ReviewNode*[n];
-
-                importaConjunto(argv[1], n, vet);
-                cout<<"Registros importados...."<<endl;
-
-                totalComparacoes = 0;
-                comparacoes = 0;
-
-                for(int i = 0; i < n; i++){
-                    arvoreVP->insere(vet[i], comparacoes);
-                    totalComparacoes += comparacoes;
-                }
+    while(opcao != 3)
+    {
+        switch(opcao)
+        {
+            case 1: // arvore vermelho e preta
+            {                
                 cout<<endl<<" ___________________SUBMENU_____________________|"<<endl;
                 cout<<"| Para analise de estruturas, digite [1]              |"<<endl;
                 cout<<"| Para modulo de testes [2]                           |"<<endl;
@@ -62,25 +49,45 @@ int main(int argc, char *argv[])
                     {
                         case 1: 
                         {                        
-                            int b = 100;
-                                                           
-                            Arvores arvores;                                    
-                                    //analise (busca de 100 conjuntos)                            
-                            arvores.analise(b); // imprime etapa 3
+                            int m = 3;
+                            ArvoreVermelhoPreto *arvoreVP = new ArvoreVermelhoPreto();
+
+                            ReviewNode **vet = new ReviewNode*[n];
+
+                            importaConjunto(argv[1], n, vet);
+                            cout<<"Registros importados...."<<endl;
+
+                            totalComparacoes = 0;
+
+                            for(int i = 0; i < 100; i++){
+                            arvoreVP->insere(vet[i], comparacoes);
+                            totalComparacoes += comparacoes;}
+                                
+                            Arvores arvores;
+                            //analise (insercao  e busca de 100 conjuntos)
+                            
+                            arvores.analise(m); // imprime etapa 3
                             cout<<"registros analisados";
+                            delete arvoreVP;
                             
                         }
                         break; 
+
                         case 2:// modulo teste
                         {
-                            char *id;
-                            cout<< "Digite o numero desejado busca na arvore:";
-                            cin>>id;
+                            char *b;
                             fstream arq;
                             arq.open("Dados.txt");
-        
+                            ArvoreVermelhoPreto *arvoreVP = new ArvoreVermelhoPreto();
+                            ReviewNode **vet = new ReviewNode*[n];
+                            importaConjunto(argv[1], n, vet);
+                            cout<<"Registros importados...."<<endl;
+                            
+                            cout<< "Digite o numero desejado busca na arvore:";
+                            cin>>b;
+                                                        
                             if(arq)
-                            {
+                            {  
                                 ofstream busca;                                
                                 busca.open("saidaBusca.txt");
                                 
@@ -91,43 +98,64 @@ int main(int argc, char *argv[])
                                         int comparacoes;
 
                                         ReviewNode **vet = new ReviewNode*[n];
-                                        high_resolution_clock::time_point inicio = high_resolution_clock::now();                                
-                                        for(int i=0;i<n;i++)
+                                        high_resolution_clock::time_point inicio = high_resolution_clock::now();                             
+                                        for(int i=0;i<n;i++){
 
-                                        arvoreVP->busca(id, comparacoes);
-                                        totalComparacoes += comparacoes;
-                                        auto resultado = high_resolution_clock::now() - inicio;
-                                        double tempo = duration_cast<nanoseconds >(resultado).count();
-                                        
-                                        
-                                        busca << "Arvore-VP: \n" << "Tempo: " << tempo/1000000000 << "\nComparações: " <<totalComparacoes << endl;
+                                            arvoreVP->insere(vet[i], comparacoes);
+                                            arvoreVP->busca(b, comparacoes);
+                                            totalComparacoes += comparacoes;
+                                            auto resultado = high_resolution_clock::now() - inicio;
+                                            double tempo = duration_cast<nanoseconds >(resultado).count();
+                                            
+                                            
+                                            busca << "Arvore-VP: \n" << "Tempo: " << tempo/1000000000 << "\nComparações: " <<totalComparacoes << endl;
 
-                                        busca << "ID" << id << endl<<endl;
+                                            busca << "ID" << b << endl<<endl;
+                                            delete arvoreVP;
+                                        }
                                         busca.close();
                                     }
                                 }else
-                                    cout << "Nao foi possivel criar o arquivo de saida da busca" << endl;
-                                    busca.close();                        
+                                    cout << "Erro ao criar o arquivo de saida da busca" << endl;
+                                    arq.close();                        
                             }else
                             cout << "Erro ao abrir arquivo de entrada" << endl;
                         }
                         break;
                     } 
-                }                
-                delete arvoreVP;
-            }
-            break;
+                }
+                   
+            }break;
+            case 2: // arvore b
+            {
+                cout<<endl<<" ___________________SUBMENU_____________________|"<<endl;
+                cout<<"| Para analise de estruturas, digite [1]              |"<<endl;
+                cout<<"| Para modulo de testes [2]                           |"<<endl;
+                cout<<"| Para finalizar digite [0]                           |"<<endl;
+                cout<<"|_____________________________________________________|"<<endl<<endl;
+                int op2;
+                        
+                cin>> op2;
+                while(op2 != 3)
+                {
+        
+                    switch(op2)
+                    {
+                        case 1: 
+                        {        
+                        }
+                    }
+                }
 
-            case 2:
+            } 
             break;
-
             default: cout << "Opcao Invalida!" << endl;
-        }
 
+        }
         imprimeMenu();
         cin >> opcao;
     }
-
-
-    return 0;
+     return 0;
 }
+    
+
